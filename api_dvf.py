ec7d16c31f896edd18e13a6f78dc7e3a5e5d6ee1
otoucas <97524845+otoucas@users.noreply.github.com>
2025-10-29 12:56:17 +0100

    Update api_dvf.py

--- api_dvf.py
+++ api_dvf.py
@@ -1,6 +1,3 @@
-import requests
-import pandas as pd
-
 def get_last_sale_price(code_postal, adresse):
-    """Simule la récupération du dernier prix de vente (à adapter avec une vraie API DVF)."""
+    """Simule la récupération du dernier prix de vente."""
     return "300 000 €"  # Valeur par défaut pour l'exemple
